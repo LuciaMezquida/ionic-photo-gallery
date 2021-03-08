bc55e88870c646216ada197cc00dcc33699889d8
Lucia Mezquida <lucia.mezquida@gmail.com>
2021-03-08 17:11:50 +0100

    Read each image from the Filesystem into base64 format

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -65,6 +65,23 @@ export class PhotoService {
       value: JSON.stringify(this.photos)
     });
   }
+  public async loadSaved() {
+    // Retrieve cached photo array data
+    const photoList = await Storage.get({ key: this.PHOTO_STORAGE });
+    this.photos = JSON.parse(photoList.value) || [];
+  
+    // Display the photo by reading into base64 format
+    for (let photo of this.photos) {
+      // Read each saved photo's data from the Filesystem
+      const readFile = await Filesystem.readFile({
+          path: photo.filepath,
+          directory: FilesystemDirectory.Data
+      });
+
+      // Web platform only: Load the photo as base64 data
+      photo.webviewPath = `data:image/jpeg;base64,${readFile.data}`;
+    }
+  }
   
   constructor() { 
 
